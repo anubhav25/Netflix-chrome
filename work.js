27a3902ae57a25698ae100adf6f2d07796d4d940
anubhav <anubhav@gnani.ai>
2019-11-30 10:12:14 +0530

    added support for primevideo

--- work.js
+++ work.js
@@ -1,24 +1,34 @@
-setInterval(
-    ()=>{
-        var skip = document.querySelector('[aria-label="Skip Intro"]');
-        var playing = document.querySelector('[aria-label="Pause"]');        
-        if (skip && playing) {
-            skip.click(); 
-            console.info('Intro Skipped.');
-            setTimeout(()=>{
-                var paused = document.querySelector('[aria-label="Play"]');        
-                if (paused) 
-                {
-                    paused.click();
-                    console.info('Play Button Clicked.');
-                }
-            },500);
-        }
-       
-        var next = document.querySelector('div.PlayIcon');
-        if (next) {
-            next.click();
-            console.info('Next Episode.')
-        }
-        }
-    ,2000);
+function getkeys() {
+  let skip, playing, paused, next;
+  if (window.location.href.includes("netflix")) {
+    skip = '[aria-label="Skip Intro"]';
+    playing = '[aria-label="Pause"]';
+    paused = '[aria-label="Play"]';
+    next = "div.PlayIcon";
+  } else {
+    skip = ".skipElement";
+    playing = ".pausedIcon";
+    paused = ".playIcon";
+    next = ".nextUpCard";
+  }
+  return [skip, playing, paused, next];
+}
+let keys = getkeys();
+setInterval(() => {
+  let [skip, playing, paused, next] = keys.map(x => document.querySelector(x));
+  console.log(getkeys());
+  if (skip && playing) {
+    skip.click();
+    console.info("Intro Skipped.");
+    setTimeout(() => {
+      if (paused) {
+        paused.click();
+        console.info("Play Button Clicked.");
+      }
+    }, 500);
+  }
+  if (next) {
+    next.click();
+    console.info("Next Episode.");
+  }
+}, 2000);
